346e7e86cb1f8db41988053c4dd14cd74fef9081
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-04-03 13:09:50 +0600

    a

--- api/connect.php
+++ api/connect.php
@@ -1,10 +1,10 @@
 <?php 
 $connect = mysqli_connect("localhost","root","","voting") or die("connection field!");
-if($connect){
-    echo"Connected!";
-}else{
-    echo "not connected!";
-}
+// if($connect){
+//     echo"Connected!";
+// }else{
+//     echo "not connected!";
+// }
 
 
 ?>
